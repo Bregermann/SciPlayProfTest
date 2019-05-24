d01f7b28ba463d2f25377b729913393641f2e668
Bregermann <bregermann@gmail.com>
2019-05-24 03:42:02 -0400

    Create Person.cs

---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+public struct Person
+{
+    [JsonProperty]
+    public int BirthYear { get; set; }
+
+    [JsonProperty]
+    public int EndYear { get; set; }
+
+    [JsonConstructor]
+    public Person(int birthYear, int endYear)
+    {
+        BirthYear = birthYear;
+        EndYear = endYear;
+    }
+
+    public int Age()
+    {
+        return EndYear - BirthYear;
+    }
+}
